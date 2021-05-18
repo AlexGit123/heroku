6fb2a655a9097310eb945e72c3992e7481cfdbe8
AlexGit123 <70524253+AlexGit123@users.noreply.github.com>
2021-05-18 15:36:16 +0200

    New page and page changes

---
+++ database/factories/FooeyFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Fooey;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class FooeyFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Fooey::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
