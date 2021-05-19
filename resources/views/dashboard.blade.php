77ee792ba837b5aaf10d05111f6b7e416c8eec1c
AlexGit123 <70524253+AlexGit123@users.noreply.github.com>
2021-05-19 17:07:46 +0200

    fixing navigation after login

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,10 +3,6 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
-
-        <h2>Home</h2>
-
-        <h2>Fooey</h2>
     </x-slot>
 
     <div class="py-12">
@@ -14,6 +10,14 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     You're logged in!
+                </div>
+
+                <div>
+                    Visit the Home Page
+                </div>
+
+                <div>
+                    Create a Fooey
                 </div>
             </div>
         </div>
