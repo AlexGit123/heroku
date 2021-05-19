7ef1ea581d4f368470cb889d14074de22c075f3c
AlexGit123 <70524253+AlexGit123@users.noreply.github.com>
2021-05-19 17:01:03 +0200

    fixing navigation after login

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,7 @@
         </h2>
 
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ route('welcome') }}
+            {{ route('views.welcome') }}
         </h2>
     </x-slot>
 
