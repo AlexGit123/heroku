f0941b5648d1864e9d1ededfc787319ab7dd3e5f
AlexGit123 <70524253+AlexGit123@users.noreply.github.com>
2021-06-15 13:30:15 +0200

    fixing dash

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,17 +8,20 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+
                 <div class="p-6 bg-white border-b border-gray-200" style="color: green">
                     You're logged in!
                 </div>
-
                 <div class="p-6 bg-white border-b border-gray-200">
                     <a href="/"> Visit the Home Page</a>
                 </div>
-
                 <div class="p-6 bg-white border-b border-gray-200">
                     <a href="{{route('fooeys.index')}}">Create a Fooey!</a>
                 </div>
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <a href="/password-management">The Importance of Password Management</a>
+                </div>
+
             </div>
         </div>
     </div>
